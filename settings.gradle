d1e0b8cbeac2d56f40f0c7339d54dbce50d066fa
marcosnunes <marcos.nunes.lph@outlook.com>
2026-09-20 08:30:47 -0300

    Initial Commit

--- settings.gradle
+++ settings.gradle
@@ -17,7 +17,13 @@ plugins {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
     }
 }
